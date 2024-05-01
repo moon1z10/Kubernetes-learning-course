6a2db9ba6bbb968aa0e51c722c4bedb9e0e5d746
moon1z10 <blue_arena@naver.com>
2024-05-01 22:57:56 +0900

    Add dockerfile

--- 02-Todo_H2_Web_Application/src/main/java/hodlene/k8s/todo_h2_web_application/TodoH2WebApplication.java
+++ 02-Todo_H2_Web_Application/src/main/java/hodlene/k8s/todo_h2_web_application/TodoH2WebApplication.java
@@ -2,12 +2,18 @@ package hodlene.k8s.todo_h2_web_application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TodoH2WebApplication {
+public class TodoH2WebApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(TodoH2WebApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TodoH2WebApplication.class);
+    }
 }
